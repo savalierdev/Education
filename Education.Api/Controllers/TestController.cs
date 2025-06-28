970ea756e36347e4d819acc174c2bfac3d71f7db
Alper Elieyi <alperelieyi59@gmail.com>
2025-06-28 22:13:44 +0300

    Added YKS Katki Hesaplama Program

---
+++ Education.Api/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Education.Api.Controllers;
+
+[Route("api/tests")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Test endpoint is working.");
+    }
+    
+    [HttpGet("error")]
+    public IActionResult GetWithError()
+    {
+        return BadRequest("This is a test error response.");
+    }
+}
---
+++ Education.YKSKatkıHesaplama/Program.cs
@@ -0,0 +1,28 @@
+﻿
+double grade9,grade10,grade11,grade12;
+
+Console.WriteLine("9. Sınıf Notunuzu Giriniz: ");
+grade9 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("10. Sınıf Notunuzu Giriniz: ");
+grade10 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("11. Sınıf Notunuzu Giriniz: ");
+grade11 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("12. Sınıf Notunuzu Giriniz: ");
+grade12 = Convert.ToDouble(Console.ReadLine());
+
+double obp = (grade9 + grade10 + grade11 + grade12) / 4;
+Console.WriteLine("Ortaöğretim Başarı Puanınız: " + obp);
+
+Console.WriteLine("Lütfen YKS Puanınızı Giriniz: ");
+double ykspuan = Convert.ToDouble(Console.ReadLine());
+
+var katsayi = obp * 0.6;
+
+double sonuc = ykspuan * katsayi;
+
+Console.WriteLine($"YKS Ek Puanlı Hesaplama Sonucu: {sonuc}");
+
+
